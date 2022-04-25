925b7f48b17304414df6c6379ef9accec75132bf
Grace9301 <97598504+Grace9301@users.noreply.github.com>
2022-04-25 19:25:42 +0900

    fetch

--- src/weather.js
+++ src/weather.js
@@ -5,7 +5,7 @@ const API_KEY = 'f24177f5fa80c5fd9c147afbb417bf0a';
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
